30e5ab822293c5ef25b2c33675ad78c59278ce37
Kitryn <snowfennard@gmail.com>
2021-03-14 22:43:02 +0800

    Implemented !gandata

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const { DISCORD_BOT_TOKEN } = require('./keys.json')
-const { get_all_mask_data_as_csv } = require('./mask_data')
+
+const winston = require('winston')
+const { format } = winston
+winston.loggers.add('default', {
+    format: format.json(),
+    transports: [
+        new winston.transports.Console()
+    ]
+})
+
+const { generate_hashmask_csv, generate_gan_v2_csv } = require('opensealib/csv_generator') 
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -10,8 +21,12 @@ client.on('ready', () => {
 client.on('message', async msg => {
     if (msg.content === '!maskdata') {
         msg.channel.send('Getting mask data, please wait...')
-        let buf = Buffer.from(await get_all_mask_data_as_csv(), 'utf8')
-        msg.channel.send(new Discord.MessageAttachment(buf, 'out.csv'))
+        let buf = Buffer.from(await generate_hashmask_csv() ,'utf8')
+        msg.channel.send(new Discord.MessageAttachment(buf, 'mask.csv'))
+    } else if (msg.content === '!gandata') {
+        msg.channel.send('Getting BGANV2 data, this may take awhile! please wait...')
+        let buf = Buffer.from(await generate_gan_v2_csv(), 'utf8')
+        msg.channel.send(new Discord.MessageAttachment(buf, 'bganv2.csv'))
     }
 })
 
